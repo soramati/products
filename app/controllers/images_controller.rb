8252d24923ae3462b7a473e12a91a480f1c434da
soramati <ishi.tokuri.asborake87@gmail.com>
2019-05-28 07:13:22 +0900

    card

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,15 +4,22 @@ class ImagesController < ApplicationController
 
 
 	def create
+		cards = Card.where(user_id: get_current_user.name)
+		card1 = cards[0]
+		text1 = card1.title
+
 		img = Magick::ImageList.new("app/assets/images/likeit.png")
 		scaled_img = img.scale(600, 314)
 
+		font = "app/assets/fonts/Makinas-4-Square.otf"
 		draw = Magick::Draw.new
-		draw.annotate(scaled_img, 0, 0, 130, 150, 'HELLO WORLD!!') do
+		draw.annotate(scaled_img, 0, 0, 50, 115, text1) do
+		  self.font      = font
 		  self.stroke    = 'transparent'
-		  self.pointsize = 40
+		  self.pointsize = 80
 		  self.gravity   = Magick::NorthWestGravity
 		end
+
 
 		scaled_img.write("app/assets/images/mojilikeit.png") # save to file
 		redirect_to "/"
