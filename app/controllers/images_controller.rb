738d486f17200cb4b62bc4e32a62a6b5395c12d8
soramati <ishi.tokuri.asborake87@gmail.com>
2019-05-28 20:45:43 +0900

    card

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,7 +7,6 @@ class ImagesController < ApplicationController
 		cards = Card.where(user_id: get_current_user.name)
 		card1 = cards[0]
 		text1 = card1.title
-
 		img = Magick::ImageList.new("app/assets/images/likeit.png")
 		scaled_img = img.scale(600, 314)
 
@@ -20,7 +19,7 @@ class ImagesController < ApplicationController
 		  self.gravity   = Magick::NorthWestGravity
 		end
 		name = get_current_user.name
-		scaled_img.write("public/images/mojilikeit#{name}.png") # save
+		scaled_img.write("public/images/#{name}.png") # save
 		redirect_to "/"
 	end
 
